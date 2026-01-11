d07240074865226c28c8f75065297a3133fd9dba
n2ilva <natanaelsantos_silva@outlook.com>
2026-01-11 16:12:22 -0300

    chore: atualizar basePath para novo nome do repositório MeuPortifolio

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/MeuRepositorio' : '',
-  assetPrefix: isProd ? '/MeuRepositorio/' : '',
+  basePath: isProd ? '/MeuPortifolio' : '',
+  assetPrefix: isProd ? '/MeuPortifolio/' : '',
   images: {
     unoptimized: true,
   },
